f6e93dfdd4fff6148f2e94d01c56d042529c7147
TedClayton1 <ted_clayton@hotmail.com>
2022-04-20 14:27:36 -0400

    Successfully added stars to this project after getting error message Rating wasn't defined. I added a Rating.js folder and the error was pointing to line 3 of the Product.js page. I deleted the word Rating then retyped it and it's working.

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from './Rating'
 
 function Product({ product }) {
   return (
@@ -16,8 +17,8 @@ function Product({ product }) {
             </a>
 
             <Card.Text as="div">
-                <div className="my-3">
-                    {product.rating} from {product.numReviews} reviews
+                <div className="my-3">                    
+                    <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
                 </div>
             </Card.Text>
 
