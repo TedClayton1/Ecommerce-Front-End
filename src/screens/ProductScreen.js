b5cc45ec1c7fcaa7909dd7e45d895a49d9a82b97
TedClayton1 <ted_clayton@hotmail.com>
2022-05-02 20:41:36 -0400

    Had the following error parsing error: Expected corresponding JSX closing tag for <Row>. The issue was I had an extra closing /Col tag on line 18. Once it was removed that error was elminated.

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,11 +1,36 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
+import products from '../products'
 
-function ProductScreen() {
-  return (
-    <div>
-        Product
-    </div>
-  )
+function ProductScreen({ match }) {
+    const product = products.find((p) => p._id == match.params.id)
+    return (
+       <div>
+          <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+          <Row>
+              <Col md={6}>
+                  <Image src={product.image} alt={product.name} fluid/>  
+              </Col>
+
+
+              <Col md={3}>
+                  <ListGroup varinat="flush">
+                      <ListGroup.Item>
+                          <h3>{product.name}</h3>
+                      </ListGroup.Item>
+
+                      <ListGroup.Item>
+                          <Review value={product.rating} text={'${product.numReviews} reviews'} color={'#f8e825'} />
+                      </ListGroup.Item>
+                  </ListGroup>
+              </Col>
+
+              <Col md={3}></Col>
+          </Row>
+       </div>
+    )
 }
 
 export default ProductScreen
