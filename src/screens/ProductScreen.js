605a0bde617e4ae9b87abef5a2f91cc5044b1ef5
TedClayton1 <ted_clayton@hotmail.com>
2022-05-04 03:19:58 -0400

    I added the disabled attribute to this code solely for the purpose of graying out the cart if a particular product isn't in stock on line 60 productScreen.js file.

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -57,7 +57,7 @@ function ProductScreen({ match }) {
                           </ListGroup.Item>
 
                           <ListGroup.Item>
-                              <Button className='btn-block' type='button'>Add to Cart</Button>
+                              <Button className='btn-block' disabled={product.countInStock == 0} type='button'>Add to Cart</Button>
                           </ListGroup.Item>
                       </ListGroup>
                   </Card>
