ab00a02aff49255468c06ca7f87cfd43850619ae
TedClayton1 <ted_clayton@hotmail.com>
2022-05-04 03:10:52 -0400

    Added button for cart on the productscreen.js page successfully.

--- src/screens/ProductScreen.js
+++ src/screens/ProductScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Row, Col, Image, ListGroup, Button, Card } from 'react-bootstrap'
+import { Row, Col, Image, ListGroup, Button, Card, ListGroupItem } from 'react-bootstrap'
 import Rating from '../components/Rating'
 import products from '../products'
 
@@ -16,18 +16,52 @@ function ProductScreen({ match }) {
 
 
               <Col md={3}>
-                  <ListGroup varinat="flush">
+                  <ListGroup variant="flush">
                       <ListGroup.Item>
                           <h3>{product.name}</h3>
                       </ListGroup.Item>
 
                       <ListGroup.Item>
-                          <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+                          <Rating value={product.rating} text={`${product.numReviews} ratings`} color={'#f8e825'} />
+                      </ListGroup.Item>
+
+                      <ListGroup.Item>
+                          Price: ${product.price}
+                      </ListGroup.Item>
+
+                      <ListGroup.Item>
+                          Description: {product.description}
                       </ListGroup.Item>
                   </ListGroup>
               </Col>
 
-              <Col md={3}></Col>
+              <Col md={3}>
+                  <Card>
+                      <ListGroup variant='flush'>
+                          <ListGroup.Item>
+                              <Row>
+                                 <Col>Price:</Col>
+                                 <Col>
+                                     <strong>${product.price}</strong>
+                                 </Col>
+                              </Row>
+                          </ListGroup.Item>
+
+                          <ListGroup.Item>
+                              <Row>
+                                 <Col>Status:</Col>
+                                 <Col>
+                                    {product.countInStock > 0 ? 'In Stock' : 'Out of stock'}
+                                 </Col>
+                              </Row>
+                          </ListGroup.Item>
+
+                          <ListGroup.Item>
+                              <Button className='btn-block' type='button'>Add to Cart</Button>
+                          </ListGroup.Item>
+                      </ListGroup>
+                  </Card>
+              </Col>
           </Row>
        </div>
     )
